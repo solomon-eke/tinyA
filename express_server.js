5de823e14bef412bdef40577d65b9c1319ee3979
solomon-eke <ekesolomon11@gmail.com>
2021-11-13 01:06:56 +0000

    added the short url

--- express_server.js
+++ express_server.js
@@ -27,6 +27,13 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params,
+    longURL: req.params.longURL,
+  };
+  res.render("urls_show", templateVars);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
