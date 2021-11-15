0b092e40ae6ec4cd5494536919c3fa09a18f59f3
Solomon Eke <ekesolomon11@gmail.com>
2021-11-14 22:46:13 -0500

    Update express_server.js

--- express_server.js
+++ express_server.js
@@ -3,12 +3,74 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const cookieParser = require("cookie-parser");
+const { uuid } = require("uuidv4");
 const PORT = 4000;
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
+const password = "purple-monkey-dinosaur"; // found in the req.params object
+const hashedPassword = bcrypt.hashSync(password, 10);
 app.set("view engine", "ejs");
 app.use(cookieParser());
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+// AUTHENTICATION HELPER FUNCTIONS
+
+const findUserByEmail = function (email, users) {
+  for (let userId in users) {
+    const user = users[userId];
+    if (email === user.email) {
+      return user;
+    }
+  }
+
+  return false;
+};
+
+const createUser = function (email, password, users) {
+  const userId = uuid().substring(0, 6);
+
+  // adding to an object
+  // objectname[key] = value
+  // Create a new user
+  users[userId] = {
+    id: userId,
+    email,
+    password: bcrypt.hashSync(myPlaintextPassword, saltRounds),
+  };
+
+  return userId;
+};
+
+const authenticateUser = function (email, password, users) {
+  // retrieve the user from the db
+  const userFound = findUserByEmail(email, users);
+
+  // compare the passwords
+  // password match => log in
+  // password dont' match => error message
+  if (userFound && userFound.password === password) {
+    return userFound;
+  }
+
+  return false;
 };
 function generateRandomString(data) {
   const result = Math.random().toString(36).substr(2, 6);
@@ -79,6 +141,82 @@ app.post("/urls/:id/delete", (req, res) => {
 
   // redirect
   res.redirect("/urls");
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = { user: null };
+
+  // display the register form
+  res.render("register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  // we need to extract the info from the body of request => req.body
+  console.log("req.body:", req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+  // If the e-mail or password are empty strings
+  if (email === " " || password === " ") {
+    return "both the email and password fields are empty, please fill out";
+  }
+  // check if that user already exist in the users db
+  // if yes, send back error message
+  // if no, we're good
+
+  // userFound can be a user object OR
+  // false
+  const userFound = findUserByEmail(email, users);
+
+  console.log("userFound:", userFound);
+
+  if (userFound) {
+    res.status(401).send("Sorry, that user already exists!");
+    return;
+  }
+
+  // userFound is false => ok register the user
+
+  const userId = createUser(email, password, users);
+
+  // Log the user => ask the browser to set a cookie with the user id
+  res.cookie("user_id", userId);
+
+  // redirect to '/quotes'
+
+  res.redirect("/urls");
+});
+
+app.get("/login", (req, res) => {
+  // if we here, we assume that the user is not logged in.
+  const templateVars = { user: null };
+
+  res.render("login", templateVars);
+});
+app.post("/login", (req, res) => {
+  // extract the email and password from the body of request => req.body
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // compare the passwords
+  // password match => log in
+  // password dont' match => error message
+
+  const user = authenticateUser(email, password, users);
+
+  if (user) {
+    // user is authenticated
+    // setting the cookie
+    res.cookie("user_id", user.id);
+
+    // redirect to /quotes
+    res.redirect("/quotes"); //=> hey browser, can you do another request => get /quotes
+    return;
+  }
+
+  // user is not authenticated => send error
+
+  res.status(401).send("Wrong credentials!");
 });
 
 app.get("/u/:shortURL", (req, res) => {
